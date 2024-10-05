518a075d77c4c06cc7f0de20421fafb007971161
Waleed Azhar <waleedazhar903@gmail.com>
2024-10-05 15:57:39 +0500

    add: connect with db

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+
 const app = express();
 
-const port = 5001;
+const PORT = 5001;
+
+mongoose.connect('mongodb://127.0.0.1:27017/crud-api').then(
+  ()=> console.log('MongoDb connected'))
+  .catch(()=> console.log('Failed to connect Db'));
+
+// connectMongoDb('mongodb://127.0.0.1:27017/crud-api')
 
 app.get('/', function (req, res) {
-    res.send('GET request to homepage')
+    res.send('GET request to Home')
   });
 
-  app.listen(port, () => {
-    console.log(`Now listening on port ${port}`);
+  app.listen(PORT, () => {
+    console.log(`Now listening on port ${PORT}`);
 }); 
